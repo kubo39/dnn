cbc44113bf2c7edc2b01bac67c059ae2266212a0
Hiroki Noda <kubo39@gmail.com>
2013-10-05 16:04:28 +0900

    packaging

--- dnn.d
+++ dnn.d
@@ -1,3 +1,5 @@
+module dnn;
+
 import std.stdio, std.math, std.random;
 
 
@@ -66,7 +68,7 @@ class NeuralNetwork {
   double[][] lastChangeHidden;
   double[][] lastChangeOutput;
   bool regression;
-  double rate1;   // learning rate
+  double rate1;
   double rate2;
 
   this(int iInputCount, int iHiddenCount, int iOutputCount, bool iRegression,
@@ -198,17 +200,4 @@ class NeuralNetwork {
     writeln("done.");
   }
 
-}
-
-
-void main() {
-  auto nn = new NeuralNetwork(2, 3, 1, true); // 入力層の数, 隠れ層の数, 出力層の数, 回帰
-  double[][] inputs = [[0.0, 0.0], [0.0, 1.0], [1.0, 0.0], [1.0, 1.0]];
-  double[][] targets = [[0.0], [1.0], [1.0], [2.0]];
-
-  nn.train(inputs, targets, 1000);
-
-  foreach (p; inputs) {
-    writeln(nn.forward(p));
-  }
 }
---
+++ main.d
@@ -0,0 +1,14 @@
+import std.stdio;
+import dnn;
+
+void main() {
+  auto nn = new NeuralNetwork(2, 3, 1, true); // 入力層の数, 隠れ層の数, 出力層の数, 回帰
+  double[][] inputs = [[0.0, 0.0], [0.0, 1.0], [1.0, 0.0], [1.0, 1.0]];
+  double[][] targets = [[0.0], [1.0], [1.0], [2.0]];
+
+  nn.train(inputs, targets, 1000);
+
+  foreach (p; inputs) {
+    writeln(nn.forward(p));
+  }
+}
